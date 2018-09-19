b17e0e4f5811099c37a08eab4df5f72f424016d3
Manny Pissanos <mpissano@gmail.com>
2018-09-19 16:44:47 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,14 @@
 def my_select(array)
  i = 0
  even_arr = []
-# if array[i] == true
+
   while i < array.length
   even_arr << yield (array[i])
    i = i + 1
   end
   even_arr
  end
-# end
+
 
 
 # (my_select(nums) do |num|
